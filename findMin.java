de9725b693ba347992d67c1ee66c0e6b7629b9c5
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-03-21 11:19:36 +0400

    Add files via upload

---
+++ findMin.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class findMin{
+  public static void main (String[] arg){
+    Scanner s=new Scanner(System.in);
+    System.out.println("Enter the first number:");
+    double first=s.nextDouble();
+    System.out.println("Enter the second number:");
+    double second=s.nextDouble();
+    System.out.println("Enter the third number:");
+    double third=s.nextDouble();
+    System.out.println("The minimum value: "+minimum(first, second, third));
+    
+    
+  }
+  public static double minimum(double first, double second, double third){
+        return Math.min(Math.min(first, second), third);
+    }
+}
+
+    
